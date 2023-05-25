6d333ec6be98555a5cdd05872b6a11fa9fb53d3f
Alan McClenaghan <AlanM@Alans-MBP.broadband>
2023-05-25 22:07:14 +0100

    Banner Component

---
+++ src/client/modules/my/banner/banner.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+import BANNER_DATA from '../../../data/bannerData'
+
+export default class Banner extends LightningElement {
+
+    heading = BANNER_DATA.heading
+    description = BANNER_DATA.description
+    bannerBtnText = BANNER_DATA.bannerBtnText
+
+}
