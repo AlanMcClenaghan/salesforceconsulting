6c57f953db182454e9b2069faa910725b3a3d98f
Alan McClenaghan <AlanM@Alans-MBP.broadband>
2023-05-27 16:23:22 +0100

    About Us Component

---
+++ src/client/data/aboutusData.js
@@ -0,0 +1,14 @@
+const ABOUTUS_DATA = {
+
+    sectionHeading:'About Salesforce Consulting',
+    description:"We have one of the region's best Salesforce consulting teams. We have helped a number of organizations identify, grow, deploy, and evolve their Salesforce investments. We are headquartered in the United Kingdom and have teams in Melbourne, New York, and Toronto.",
+    tagline:'What sets us apart is our dedication to fully comprehending your requirements prior to beginning growth.',
+    stats:[
+        {label:"Satisfied Clients",value:"50+"},
+        {label:"Certifications",value:"100+"},
+        {label:"Years Completed",value:"5+"}
+    ]
+    
+}
+
+export default ABOUTUS_DATA
---
+++ src/client/modules/my/aboutus/aboutus.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import ABOUTUS_DATA from '../../../data/aboutusData'
+
+export default class Aboutus extends LightningElement {
+
+    aboutusData = ABOUTUS_DATA
+
+}
