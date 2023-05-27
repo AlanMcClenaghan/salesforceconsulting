dbfd9320b9db9e43012d5be3a75432c549bb3240
Alan McClenaghan <AlanM@Alans-MBP.broadband>
2023-05-27 17:53:33 +0100

    Our Work Component

---
+++ src/client/modules/my/ourwork/ourwork.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import OUR_WORK_DATA from '../../../data/ourworkData'
+
+export default class Ourwork extends LightningElement {
+    
+    ourworkData = OUR_WORK_DATA
+    
+}
