47d58dcfe41a25e98f4dfe97c61ee713880c994c
Alan McClenaghan <AlanM@Alans-MBP.broadband>
2023-05-28 18:48:50 +0100

    Nodemailer Setup and API Creation and Integration of LWC to Express Send Email API

--- src/client/modules/my/contactus/contactus.js
+++ src/client/modules/my/contactus/contactus.js
@@ -1,4 +1,5 @@
 import { LightningElement } from 'lwc';
+
 export default class Contactus extends LightningElement {
 
     formData = {}
@@ -8,9 +9,27 @@ export default class Contactus extends LightningElement {
         this.formData[name] = value
     }
 
-    sendEmail() {
+    sendEmail(){
         console.log(this.formData)
-        this.resetForm()
+        fetch('http://localhost:3002/api/v1/sendemail', {
+            method:'POST',
+            headers:{
+                'accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify(this.formData)
+        }).then(response=>{
+            if(!response.ok){
+                throw new Error('No response from server')
+            }
+            return response.json()
+        }).then(result=>{
+            console.log("Message sent", result)
+            this.resetForm()
+        }).catch(error=>{
+            console.error(error)
+        })
+        
     }
 
     resetForm() {
